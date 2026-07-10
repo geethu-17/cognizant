f98f5f7e76895f8f40785732f9ab982b10e936c7
Addanki.Geetika <geethikageethika48@gmail.com>
2026-07-10 22:37:11 +0530

    Add files via upload

---
+++ deepskill/Week 2/EF Core 8.0/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventory.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=RetailInventoryDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}

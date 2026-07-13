e3683f238390f77cd6ea2ec7c214b08f407bf68f
Addanki.Geetika <geethikageethika48@gmail.com>
2026-07-13 08:28:42 +0530

    Add files via upload

---
+++ deepskill/Week 2/FirstWebAPI/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.IO;
+
+namespace FirstWebAPI.Filters
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            File.AppendAllText("ErrorLog.txt",
+                $"{DateTime.Now}: {context.Exception.Message}\n");
+
+            context.Result = new ObjectResult("Internal Server Error")
+            {
+                StatusCode = 500
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
